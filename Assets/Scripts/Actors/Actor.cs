88de6b011af23275496707bb7c1be439a5a6a31b
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-06-09 00:54:51 +0300

    initial work

---
+++ Assets/Scripts/Actors/Actor.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Actor : MonoBehaviour
+{
+    public Vector3 position;
+    public int pixelWidth;
+    public int pixelHeight;
+    public Vector2 center;
+    public int tileSize;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
