b9cfdf075e0578c3787f7c72f2e19aa3ede8203f
adrianpoplesanu <adrian.poplesanu@yahoo.com>
2024-07-27 10:01:52 +0300

    added simple sprite generator of tiles

--- Assets/Scripts/Board/BoardController.cs
+++ Assets/Scripts/Board/BoardController.cs
@@ -8,6 +8,17 @@ public class BoardController : MonoBehaviour
     public int width = 10;
     public int height = 10;
     public int[,] tiles;
+    public GameObject[,] renderingTiles;
+    public Sprite newSpriteTile0;
+    public Sprite newSpriteTile1;
+    public Sprite newSpriteTile2;
+    public Sprite newSpriteTile3;
+    public Sprite newSpriteTile4;
+    public Sprite newSpriteTile5;
+    public Sprite newSpriteTile6;
+    public Sprite newSpriteTile7;
+    public Sprite newSpriteTile8;
+    public Sprite newSpriteTile9;
 
     void Start()
     {
@@ -34,9 +45,45 @@ public class BoardController : MonoBehaviour
 
     void GenerateRandomBoard() {
         tiles = new int[width, height];
+        renderingTiles = new GameObject[width, height];
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < height; j++) {
-                tiles[i, j] = 1;
+                tiles[i, j] = Random.Range(0, 10);
+                renderingTiles[i, j] = new GameObject();
+                SpriteRenderer spriteRenderer = renderingTiles[i, j].AddComponent<SpriteRenderer>();
+                switch(tiles[i, j]){
+                    case 0:
+                        spriteRenderer.sprite = newSpriteTile0;
+                    break;
+                    case 1:
+                        spriteRenderer.sprite = newSpriteTile1;
+                    break;
+                    case 2:
+                        spriteRenderer.sprite = newSpriteTile2;
+                    break;
+                    case 3:
+                        spriteRenderer.sprite = newSpriteTile3;
+                    break;
+                    case 4:
+                        spriteRenderer.sprite = newSpriteTile4;
+                    break;
+                    case 5:
+                        spriteRenderer.sprite = newSpriteTile5;
+                    break;
+                    case 6:
+                        spriteRenderer.sprite = newSpriteTile6;
+                    break;
+                    case 7:
+                        spriteRenderer.sprite = newSpriteTile7;
+                    break;
+                    case 8:
+                        spriteRenderer.sprite = newSpriteTile8;
+                    break;
+                    case 9:
+                        spriteRenderer.sprite = newSpriteTile9;
+                    break;
+                }
+                renderingTiles[i, j].transform.position = new Vector3(i, j, 0);
             }
         }
     }
